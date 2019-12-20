fedf7ab932f4a381c08803652eddfe9867ce353e
Nelson Alexandre Saraiva Gregorio <up200900303@gnomo.fe.up.pt>
2019-12-20 01:27:05 +0000

    Change database interface to REST api implementation

--- src/testSensorPI.py
+++ src/testSensorPI.py
@@ -1,94 +1,37 @@
 import RPi.GPIO as GPIO
 import time
 import datetime
-import sqlite3
 import sys
+import time
+import random
 
-conn = None
-db_cursor = None
-
-check_room_query = 'SELECT roomID FROM Room WHERE roomID=?'
-add_room_query = """INSERT INTO 'Room' ('roomID', 'roomName', 'roomSeats', 'roomAvailable') VALUES(?, ?, ?, ?);"""
-add_entry_query = """INSERT INTO 'Entries' ('timestamp', 'is_entry', 'room') values(?, ?, ?);"""
-
-def commit_database():
-    if(conn != None):
-        conn.commit()
-
-def exec_query(query, data_tuple):
-    if(connect_database()):
-        db_cursor.execute(query, data_tuple)
-        return True
-    else:
-        return False
-
-def connect_database():
-    global conn
-    global db_cursor
-
-    if(conn == None):
-        conn = sqlite3.connect('calfred.db')
-        if(db_cursor != None): db_cursor.close()
-        db_cursor = conn.cursor()
-    elif(db_cursor == None):
-        db_cursor = conn.cursor()
-
-    return (db_cursor != None)
-
-def close_database():
-    if(db_cursor != None):
-        db_cursor.close()
-
-    if(conn != None):
-        db_cursor.close()
-
-def check_room(roomID):
-    if(exec_query(check_room_query, (roomID,))):
-        got_room = db_cursor.fetchone()
-        print(got_room)
-        return (got_room != None)
-    else:
-        return False
-
-def add_room(roomID, roomName, roomSeats):
-    if(not check_room(roomID)):
-        room_data = (roomID, roomName, roomSeats, roomSeats)
-        if(exec_query(add_room_query, room_data)):
-            conn.commit()
-            return True
-    return False
-
-def add_entry(timestamp, is_entry, roomID):
-    entry_data = (timestamp, is_entry, roomID)
-    if(exec_query(add_entry_query, entry_data)):
-        conn.commit()
-        return True
-    return False
+# importing the requests library 
+import requests 
 
 
 
+def check_talk(talkID):
+	r = requests.get("https://web.fe.up.pt/~up200900303/open-cx-countercode/CounterCodeWeb/rest/get_talk_data.php?id="+talkID)
+	reply = r.json()
+	return (reply['response_desc'] == 'OK')
 
-
-connect_database()
+def add_entry(talkID, timestamp, is_entry):
+	pload = {'talkID':talkID, 'timestamp':timestamp, 'is_entry':is_entry}
+	r = requests.post("https://web.fe.up.pt/~up200900303/open-cx-countercode/CounterCodeWeb/rest/insert_entry.php", data = pload)
+	reply = r.json()
+	if(reply['response_desc'] != 'OK'):
+		print("Failed to add entry")
 
 argc = len(sys.argv)
 
 if(argc == 2):
-    check_room(sys.argv[1])
-elif(argc == 4):
-    add_room(sys.argv[1], sys.argv[2], sys.argv[3])
+	if(not check_talk(sys.argv[1])):
+		print ("ERROR: Talk {:s} doesn't exist".format(sys.argv[1]))
+		exit(-1)
 else:
-    print ("USAGE: {:s} <roomID>".format(sys.argv[0]))
-    print ("USAGE: {:s} <roomID> <roomName> <roomSeats>".format(sys.argv[0]))
+	print ("USAGE: {:s} <talkID>".format(sys.argv[0]))
+	exit(-1)
 
-
-# Database
-# sqlite_insert_with_param = """INSERT INTO 'Entries'
-#                           ('timestamp', 'is_entry') 
-#                           VALUES (?, ?);"""
-
-# sqlite_insert_with_param = """INSERT INTO 'Entries' ('timestamp', 'is_entry', 'room')
-# values(?, ?, ?);"""
 
 probePin = 18
 probePin_btn = 17
@@ -122,94 +65,38 @@ pulse = 0
 pulseTimer = 0.1
 
 def statemachine(pin1, pin2):
-    global state
-    if (state == IDLE):
-        if(pin1 == pin2): return
-        state = S1 if (pin1 > pin2) else S2
-    elif (state == S1):
-        if(pin2 == 1):
-            print("Entrada")
-            add_entry(datetime.datetime.now(), 1, sys.argv[1])
-            # data_tuple = (datetime.datetime.now(), 1, sys.argv[1])
-            # db_cursor.execute(sqlite_insert_with_param, data_tuple)
-            # conn.commit()
-            state = IDLE
-        elif(pin1 == 0):
-            state = IDLE
-    elif (state == S2):
-        if(pin1 == 1):
-            print("Saida")
-            add_entry(datetime.datetime.now(), 0, sys.argv[1])
-            # data_tuple = (datetime.datetime.now(), 0, sys.argv[1])
-            # db_cursor.execute(sqlite_insert_with_param, data_tuple)
-            # conn.commit()
-            state = IDLE
-        elif(pin2 == 0):
-            state = IDLE
-    else:
-        pass
-
-    # global state
-    # if (state == IDLE):
-    #     print("IDLE")
-    #     if(pin1 == 1 and pin2 == 0):
-    #         state = S1
-    #     elif(pin1 == 0 and pin2 == 1):
-    #         state = S2
-    # elif (state == S1):
-    #     print("S1")
-    #     if(pin2 == 1):
-    #         print("Entrada")
-    #         state = IDLE
-    #     elif(pin1 == 0):
-    #         state = IDLE
-    # elif (state == S2):
-    #     print("S2")
-    #     if(pin1 == 1):
-    #         print("Saida")
-    #         state = IDLE
-    #     elif(pin2 == 0):
-    #         state = IDLE
-    # else:
-    #     pass
-
+	global state
+	if (state == IDLE):
+		if(pin1 == pin2): return
+		state = S1 if (pin1 > pin2) else S2
+	elif (state == S1):
+		if(pin2 == 1):
+			print("Entrada")
+			add_entry(sys.argv[1], datetime.datetime.now(), 'true')
+			state = IDLE
+		elif(pin1 == 0):
+			state = IDLE
+	elif (state == S2):
+		if(pin1 == 1):
+			print("Saida")
+			add_entry(sys.argv[1], datetime.datetime.now(), 'false')
+			state = IDLE
+		elif(pin2 == 0):
+			state = IDLE
+	else:
+		pass
 
 while(timerElapsed < timer ):
-    tNow = time.time()
-    deltaTime = tNow - lastTime
-    lastTime = tNow
-    timerElapsed += deltaTime
+	tNow = time.time()
+	deltaTime = tNow - lastTime
+	lastTime = tNow
+	timerElapsed += deltaTime
 
-    pulse += deltaTime
-    if(pulse >= pulseTimer):
-        pulse = 0
-        pin = GPIO.input(probePin)
-        pin_btn = GPIO.input(probePin_btn)
+	pulse += deltaTime
+	if(pulse >= pulseTimer):
+		pulse = 0
+		pin = GPIO.input(probePin)
+		pin_btn = GPIO.input(probePin_btn)
 
-        statemachine(pin, pin_btn)
-        print ("State: {:4s} | S: {:1s} | B:  {:1s}".format(state_names.get(state), str(pin), str(pin_btn)))
-
-
-        # if(pin == 1):
-        #     count1 += 1
-        # elif(pin == 0):
-        #     count0 += 1
-
-        # if(state == IDLE and pin == 1):
-        #     state = S1
-        #     start = time.time()
-        #     print("IDLE to S1")
-        
-        # elif (state == S1 and pin == 0):
-        #     end = time.time()
-        #     moveTime = end - start
-        #     if(moveTime > 0.001):
-        #         state = IDLE
-        #         print("S1 to idle")
-        #         print("MOVE" + str(moveTime))
-
-                # data_tuple = (datetime.datetime.now(), 1)
-                # db_cursor.execute(sqlite_insert_with_param, data_tuple)
-                # conn.commit()
-
-close_database()
+		statemachine(pin, pin_btn)
+		print ("State: {:4s} | S: {:1s} | B:  {:1s}".format(state_names.get(state), str(pin), str(pin_btn)))
